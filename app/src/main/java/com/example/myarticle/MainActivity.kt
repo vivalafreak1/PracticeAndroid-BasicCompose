ff4480ead4c8cb6b8f982fbe2697d0dd50637100
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2022-10-20 07:55:58 +0700

    Finalize

--- app/src/main/java/com/example/myarticle/MainActivity.kt
+++ app/src/main/java/com/example/myarticle/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.myarticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myarticle.ui.theme.MyArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Article(title = stringResource(R.string.title), intro = stringResource(R.string.intro), body = stringResource(R.string.body))
                 }
             }
         }
@@ -30,14 +38,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Article(title: String, intro:String, body: String) {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Column{
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(),
+            //contentScale = ContentScale.Crop
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .wrapContentWidth(align = Alignment.Start )
+                .padding(16.dp)
+        )
+        Text(
+            text = intro,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .wrapContentWidth(align = Alignment.Start)
+                .padding(16.dp)
+        )
+        Text(
+            text = body,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .wrapContentWidth(align = Alignment.Start)
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ArticlePreview() {
     MyArticleTheme {
-        Greeting("Android")
+        Article(title = stringResource(R.string.title), intro = stringResource(R.string.intro), body = stringResource(R.string.body))
     }
 }
