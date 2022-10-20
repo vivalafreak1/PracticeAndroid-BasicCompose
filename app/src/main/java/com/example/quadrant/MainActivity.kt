a55e44e31314dbebd500874ee6c49829425f5b9c
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2022-10-20 22:14:10 +0700

    Finalizing

--- app/src/main/java/com/example/quadrant/MainActivity.kt
+++ app/src/main/java/com/example/quadrant/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.quadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.quadrant.ui.theme.QuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Quadrant()
                 }
             }
         }
@@ -30,14 +37,73 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Quadrant() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInforCard(
+                title = stringResource(R.string.title_green),
+                desc = stringResource(R.string.description_green),
+                backgroundColor = Color.Green,
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInforCard(
+                title = stringResource(R.string.title_yellow),
+                desc = stringResource(R.string.description_yellow),
+                backgroundColor = Color.Yellow,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableInforCard(
+                title = stringResource(R.string.title_blue),
+                desc = stringResource(R.string.description_blue),
+                backgroundColor = Color.Blue,
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInforCard(
+                title = stringResource(R.string.title_silver),
+                desc = stringResource(R.string.description_silver),
+                backgroundColor = Color.Cyan,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
 }
+
+@Composable
+fun ComposableInforCard(
+    title: String,
+    desc: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+        )
+        Text(
+            text = desc,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun QuadrantPreview() {
     QuadrantTheme {
-        Greeting("Android")
+        Quadrant()
     }
 }
